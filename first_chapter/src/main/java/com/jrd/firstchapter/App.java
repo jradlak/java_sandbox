3156bc1343c1dfe84994b3daf1bfba582eb4f028
Jakub Radlak <jradlak@gmail.com>
2016-03-22 19:53:43 +0100

    inicjalizacja projektu, dodanie plików pierwszego zadania

---
+++ first_chapter/src/main/java/com/jrd/firstchapter/App.java
@@ -0,0 +1,13 @@
+package com.jrd.firstchapter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
