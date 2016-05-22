696bc02f127b3ba40891af23c394ad10d4fe94e8
Jakub Radlak <jradlak@gmail.com>
2016-05-22 22:51:20 +0200

    rozwiązanie zadania 8 z rozdziału 3

--- third_chapter/src/main/java/com/jrd/third_chapter/App.java
+++ third_chapter/src/main/java/com/jrd/third_chapter/App.java
@@ -6,5 +6,18 @@ package com.jrd.third_chapter;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello third chapter!!!");
+
+        greeterTest();
+    }
+
+    private static void greeterTest() {
+        Runnable gr1 = new Greeter(1000, "Chewbacca");
+        Runnable gr2 = new Greeter(1000, "Sokół Millennium");
+
+        Thread th1 = new Thread(gr1);
+        Thread th2 = new Thread(gr2);
+
+        th1.start();
+        th2.start();
     }
 }
