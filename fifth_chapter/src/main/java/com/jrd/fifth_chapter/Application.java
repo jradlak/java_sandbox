a1fd8ea7b086398361d013176b5c02b843a1c95a
Jakub Radlak <jradlak@gmail.com>
2016-06-20 21:09:03 +0200

    inicjalizacja projektu dla piątego rozdziału

---
+++ fifth_chapter/src/main/java/com/jrd/fifth_chapter/Application.java
@@ -0,0 +1,10 @@
+package com.jrd.fifth_chapter;
+
+/**
+ * Created by jakub on 20.06.16.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("No elo!");
+    }
+}
---
+++ fifth_chapter/src/main/java/com/jrd/fifth_chapter/ReadOperations/DoubleFileReader.java
@@ -0,0 +1,7 @@
+package com.jrd.fifth_chapter.ReadOperations;
+
+/**
+ * Created by jakub on 20.06.16.
+ */
+public class DoubleFileReader {
+}
