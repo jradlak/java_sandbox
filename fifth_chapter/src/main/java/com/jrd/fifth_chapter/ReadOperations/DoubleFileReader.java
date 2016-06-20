9dba76360d2d4e2273c89c94164151704077163d
Jakub Radlak <jradlak@gmail.com>
2016-06-20 22:04:27 +0200

    rozwiązanie zadania 1 z rozdziału 5

--- fifth_chapter/src/main/java/com/jrd/fifth_chapter/ReadOperations/DoubleFileReader.java
+++ fifth_chapter/src/main/java/com/jrd/fifth_chapter/ReadOperations/DoubleFileReader.java
@@ -1,7 +1,26 @@
 package com.jrd.fifth_chapter.ReadOperations;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by jakub on 20.06.16.
+ *
+ * Rozwiązanie zadania 1:
+ * Przeczytanie pliku zawierającego liczby double i zwrócenie listy tych liczb.
+ * Jeśli konieczne, wyrzucanie stosownych wyjątków w zależności od sytuacji.
  */
 public class DoubleFileReader {
+    public List<Double> readValues(String fileName) throws IOException, NumberFormatException {
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+            return lines.map(v -> Double.parseDouble(v))
+                    .collect(Collectors.toList());
+        } catch (IOException | NumberFormatException e) {
+            throw e;
+        }
+    }
 }
--- fifth_chapter/src/test/java/com/jrd/fifth_chapter/ReadOperations/DoubleFileReaderTest.java
+++ fifth_chapter/src/test/java/com/jrd/fifth_chapter/ReadOperations/DoubleFileReaderTest.java
@@ -1,7 +1,32 @@
 package com.jrd.fifth_chapter.ReadOperations;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by jakub on 20.06.16.
  */
 public class DoubleFileReaderTest {
+
+    private DoubleFileReader reader;
+
+    @Before
+    public void setup() {
+        reader = new DoubleFileReader();
+    }
+
+    @Test
+    public void readDoubleValuesTest() throws IOException {
+        List<Double> values = reader.readValues("doubles1.txt");
+        Assert.assertTrue(values.size() > 0);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void readNonNumberTest() throws NumberFormatException, IOException {
+        reader.readValues("doublesErr.txt");
+    }
 }
